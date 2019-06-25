884d18fbb7eeccc5c3e9582ca4171b06464071d1
awygle <awygle@gmail.com>
2019-06-25 08:28:24 -0700

    Load pifrom into a Memory struct.
    Needs error handling, which will enable tests.

--- src/bin/magic.rs
+++ src/bin/magic.rs
@@ -1,8 +1,23 @@
-use magic::EmuOptions;
 use gumdrop::Options;
+use magic::*;
+use std::error::Error;
+use std::fs::File;
+use std::path::Path;
 
 fn main() {
     let opts = EmuOptions::parse_args_default_or_exit();
-    
+    let pifrom_path = Path::new(&opts.pifrom_path);
+
+    let mut file = match File::open(&pifrom_path) {
+        Err(why) => panic!(
+            "Couldn't open pifrom file {}: {}",
+            pifrom_path.display(),
+            why.description()
+        ),
+        Ok(file) => file,
+    };
+
+    let _memory = Memory::new(&mut file);
+
     println!("{:#?}", opts);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,43 @@
 use gumdrop::Options;
+use std::error::Error;
 
 #[derive(Debug, Options)]
 pub struct EmuOptions {
     #[options(help = "print help message")]
     help: bool,
     #[options(help = "path to PIF boot ROM", long = "pifrom", required)]
-    pifrom_path: String,
+    pub pifrom_path: String,
+}
+
+pub struct Memory {
+    pifrom: [u8; 2048],
+}
+
+pub trait MemoryBus {
+    fn new(pifrom_src: &mut impl std::io::Read) -> Self;
+}
+
+impl MemoryBus for Memory {
+    fn new(pifrom_src: &mut impl std::io::Read) -> Self {
+        let mut result = Memory { pifrom: [0; 2048] };
+        if let Err(why) = pifrom_src.read(&mut result.pifrom) {
+            panic!("Couldn't read pifrom: {}", why.description());
+        }
+        result
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use gumdrop::{Parser, ParsingStyle,Opt};
+    use gumdrop::ParsingStyle;
     #[test]
     fn pifrom_present() {
         let args: &[&str] = &["--pifrom=pifdata.bin"];
         let opts = EmuOptions::parse_args(args, ParsingStyle::AllOptions).unwrap();
         assert_eq!(opts.pifrom_path, "pifdata.bin");
     }
-    
+
     #[test]
     #[should_panic]
     fn pifrom_required() {
@@ -26,10 +45,10 @@ mod tests {
         let opts = EmuOptions::parse_args(args, ParsingStyle::AllOptions).unwrap();
         assert_eq!(opts.pifrom_path, "pifdata.bin");
     }
-    
+
     #[test]
     fn help_no_pifrom() {
         let args: &[&str] = &["--help"];
-        let opts = EmuOptions::parse_args(args, ParsingStyle::AllOptions).unwrap();
+        let _opts = EmuOptions::parse_args(args, ParsingStyle::AllOptions).unwrap();
     }
 }
