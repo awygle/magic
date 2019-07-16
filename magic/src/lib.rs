ac99c1de51956380bf499516396de438ca8a3bce
Atul Bhosale <atul1bhosale@gmail.com>
2019-07-16 23:25:17 +0530

    Format code using 'cargo fmt'

--- magic/src/lib.rs
+++ magic/src/lib.rs
@@ -1,8 +1,8 @@
-use gumdrop::Options;
-use std::error::Error;
-use std::collections::VecDeque;
 use arraydeque::{ArrayDeque, Saturating};
-use ux::{u20, u14};
+use gumdrop::Options;
+use std::collections::VecDeque;
+use std::error::Error;
+use ux::{u14, u20};
 
 #[derive(Debug, Options)]
 pub struct EmuOptions {
@@ -48,14 +48,14 @@ impl Default for ICacheLine {
     }
 }
 
-pub trait InstructionCache : Default {
+pub trait InstructionCache: Default {
     type AddressSize;
     type Output;
     fn fetch(&self, addr: Self::AddressSize) -> Self::Output;
 }
 
 pub struct ICache {
-    memory: [ICacheLine; 512]
+    memory: [ICacheLine; 512],
 }
 
 impl Default for ICache {
@@ -79,16 +79,17 @@ enum CpuCommand {
     WaitMem(u8),
 }
 
-pub struct InterpCPU32bit<MB, IC, MM> 
-    where MB: MemoryBus, 
+pub struct InterpCPU32bit<MB, IC, MM>
+where
+    MB: MemoryBus,
     IC: InstructionCache,
-    MM: MMU
+    MM: MMU,
 {
     pc: u32,
     bus: MB,
     icache: IC,
     mmu: MM,
-    cmd_queue: VecDeque<ArrayDeque<[CpuCommand; 8], Saturating>>
+    cmd_queue: VecDeque<ArrayDeque<[CpuCommand; 8], Saturating>>,
 }
 
 pub trait CPU {
@@ -99,8 +100,9 @@ pub trait CPU {
 const RESET_VECTOR_32: u32 = 0xBC00_0000;
 const MEM_WORD_DELAY: usize = 38;
 
-impl<MB: MemoryBus, IC: InstructionCache, MM: MMU> CPU for InterpCPU32bit<MB, IC, MM> 
-where MM::AddressSize : From<u32>
+impl<MB: MemoryBus, IC: InstructionCache, MM: MMU> CPU for InterpCPU32bit<MB, IC, MM>
+where
+    MM::AddressSize: From<u32>,
 {
     fn new(pifrom_src: &mut impl std::io::Read) -> Self {
         InterpCPU32bit {
@@ -111,21 +113,19 @@ where MM::AddressSize : From<u32>
             cmd_queue: VecDeque::with_capacity(64),
         }
     }
-    
+
     /*fn instruction_fetch(&mut self, _: CpuCommand) {
         if !self.mmu.is_cached(self.pc) {
             self.bus.fetch(self.pc);
             self.cmd_queue.push_back(CpuCommand::WaitMem(MEM_WORD_DELAY));
         }
     }*/
-    
+
     fn run(mut self) {
         loop {
             while let Some(cmd) = self.cmd_queue.pop_front() {
                 match cmd {
-                    InstructionFetch => {
-                        
-                    }
+                    InstructionFetch => {}
                 }
             }
         }
@@ -138,14 +138,12 @@ impl<MB: MemoryBus, IC: InstructionCache, MM: MMU> InterpCPU32bit<MB, IC, MM> {
     }
 }
 
-pub trait MMU : Default {
+pub trait MMU: Default {
     type AddressSize;
     fn is_cached(addr: Self::AddressSize) -> bool;
 }
 
-pub struct MMU32Bit {
-    
-}
+pub struct MMU32Bit {}
 
 impl Default for MMU32Bit {
     fn default() -> Self {
@@ -155,7 +153,7 @@ impl Default for MMU32Bit {
 
 impl MMU for MMU32Bit {
     type AddressSize = u32;
-    
+
     fn is_cached(addr: u32) -> bool {
         addr >= 0xA000_0000 && addr < 0xC000_0000
     }
