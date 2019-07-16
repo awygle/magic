37f96fe7455002fa3f1dd29159075e94df3bb289
awygle <awygle@gmail.com>
2019-07-15 19:57:48 -0700

    Initial proc macro template

--- magic-macros/src/lib.rs
+++ magic-macros/src/lib.rs
@@ -177,7 +177,7 @@ impl<'de> Deserialize<'de> for RegNum {
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(default)]
-pub struct MetaInstruction {
+struct MetaInstruction {
     name: String,
     #[serde(rename = "type")]
     itype: InstructionType,
@@ -277,4 +277,29 @@ mod tests {
         let except :Exception = serde_json::from_str(r#""overflow""#).unwrap();
         assert_eq!(except, Exception::Overflow);
     }
+}
+
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use proc_macro2;
+use syn;
+use quote::quote;
+use std::fs::File;
+use std::io::prelude::*;
+
+#[proc_macro]
+pub fn forall_instructions(code: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(code as syn::LitStr);
+    println!("opening {} from {:?}", input.value(), std::env::current_dir().unwrap());
+    let mut f = File::open(&input.value()).unwrap();
+    let mut src = String::new();
+    f.read_to_string(&mut src);
+    let instrs :Vec<MetaInstruction> = serde_json::from_str(&src).unwrap();
+    let names = instrs.into_iter().map(|x| syn::Ident::new(&x.name.replace(".", "_"), proc_macro2::Span::call_site()));
+    let result = quote! {
+        #(
+        enum #names {}
+        )*
+    };
+    result.into()
 }
